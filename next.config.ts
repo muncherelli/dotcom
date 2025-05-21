af11987c870a2abc924e2ec801c5681137a3efd4
muncherelli <16467105+muncherelli@users.noreply.github.com>
2025-05-21 18:38:45 -0500

    added landing page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        pathname: "/**",
+      },
+    ],
+  },
   /* config options here */
 };
 
